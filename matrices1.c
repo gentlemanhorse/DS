fd5b5db24b8a98bff9bb42c9ea612fdb68c29336
gentlemanhorse <138687107+gentlemanhorse@users.noreply.github.com>
2024-03-13 12:28:13 -0300

    Update matrices1.c

--- matrices1.c
+++ matrices1.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
-{
-    int col, lin;
+void tamMat(int* lin, int* col){
     printf("Digite a  largura da matriz: ");
-    scanf("%d", &lin);
+    scanf("%d", lin);
     printf("Digite a altura da matriz: ");
-    scanf("%d", &col);
-    
+    scanf("%d", col);
+}
+
+int** initVetP(int lin, int col){
     int **vetP = malloc(lin * sizeof(int*));
     
     for(int i = 0; i < lin; i++)
@@ -21,21 +21,36 @@ int main()
         }
     }
     
+    return vetP;
+}
+
+void impVetP(int** vetP, int lin, int col){
     for(int i = 0; i < lin; i++){
-        for(int j = 0; j < col; j++){
+        for(int j = 0; j < col; j++)
             printf("%d ", vetP[i][j]);
-        }
         printf("\n");
     }
     
     printf("Transposta:\n");
     for(int i = 0; i < col; i++){
-        for(int j = 0; j < lin; j++){
+        for(int j = 0; j < lin; j++)
             printf("%d ", vetP[j][i]);
-        }
         printf("\n");
     }
-    
+}
 
+int main()
+{
+    int col, lin;
+    tamMat(&lin, &col);
+    int **vetP = initVetP(lin, col);
+    impVetP(vetP, lin, col);
+    
+    
+    
+    
+    
+    
+    free(vetP);
     return 0;
 }
