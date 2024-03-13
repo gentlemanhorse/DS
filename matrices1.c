a3d2153b6d66e72081b1c231c6860b89ed33b887
gentlemanhorse <138687107+gentlemanhorse@users.noreply.github.com>
2024-03-13 11:59:37 -0300

    Create matrices1.c

---
+++ matrices1.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int col, lin;
+    printf("Digite a  largura da matriz: ");
+    scanf("%d", &lin);
+    printf("Digite a altura da matriz: ");
+    scanf("%d", &col);
+    
+    int **vetP = malloc(lin * sizeof(int*));
+    
+    for(int i = 0; i < lin; i++)
+        vetP[i] = malloc(col * sizeof(int*));
+        
+    for(int i = 0; i < lin; i++){
+        for(int j = 0; j < col; j++){
+            printf("Digite o elemento vetP[%d][%d] ", i + 1, j + 1);
+            scanf("%d", &vetP[i][j]);
+        }
+    }
+    
+    for(int i = 0; i < lin; i++){
+        
+    }
+    
+
+    return 0;
+}
