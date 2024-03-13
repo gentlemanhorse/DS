13857a6ceeae030e550a460e6c66fd5436e32483
gentlemanhorse <138687107+gentlemanhorse@users.noreply.github.com>
2024-03-13 13:06:26 -0300

    Update matrices1.c

--- matrices1.c
+++ matrices1.c
@@ -2,9 +2,9 @@
 #include <stdlib.h>
 
 void tamMat(int* lin, int* col){
-    printf("Digite a  largura da matriz: ");
+    printf("Digite a  altura da matriz: ");
     scanf("%d", lin);
-    printf("Digite a altura da matriz: ");
+    printf("Digite a largura da matriz: ");
     scanf("%d", col);
 }
 
@@ -16,7 +16,7 @@ int** initVetP(int lin, int col){
         
     for(int i = 0; i < lin; i++){
         for(int j = 0; j < col; j++){
-            printf("Digite o elemento vetP[%d][%d] ", i + 1, j + 1);
+            printf("Digite o elemento [%d][%d]\n>>> ", i + 1, j + 1);
             scanf("%d", &vetP[i][j]);
         }
     }
@@ -39,18 +39,51 @@ void impVetP(int** vetP, int lin, int col){
     }
 }
 
+int* initVetU(int lin, int col){
+    int* mat = malloc(lin * col * sizeof(int));
+    
+    for(int i = 0; i < lin; i++){
+        for(int j = 0; j < col; j++){
+            printf("lin = %d, col = %d\n", lin, col);
+            printf("Digite o valor do elemento [%d][%d]\n>>> ", i, j, (i * col) + j);
+            scanf("%d", &mat[(i * col) + j]);
+        }
+    }
+    
+    return mat;
+}
+
+void impVetU(int* mat, int lin, int col){
+    for(int i = 0; i < lin; i++){
+        for(int j = 0; j < col; j++)
+            printf(" %d", mat[(i * col) + j]);
+        printf("\n");
+    }
+    
+    printf("Transposta:\n");
+    for(int i = 0; i < col; i++){
+        for(int j = 0; j < lin; j++)
+            printf(" %d", mat[(j * col) + i]);
+        
+    printf("\n");
+    }
+}
+
 int main()
 {
+    // Vetor de ponteiros
     int col, lin;
     tamMat(&lin, &col);
     int **vetP = initVetP(lin, col);
     impVetP(vetP, lin, col);
     
-    
-    
-    
+    // Aloc única
+    tamMat(&lin, &col);
+    int *mat = initVetU(lin, col);
+    impVetU(mat, lin, col);
     
     
     free(vetP);
+    free(mat);
     return 0;
 }
