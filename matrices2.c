b4818bfde40ad6a22baff611db3fe7c7c69908b7
gentlemanhorse <138687107+gentlemanhorse@users.noreply.github.com>
2024-03-18 13:02:06 -0300

    Update matrices2.c

--- matrices2.c
+++ matrices2.c
@@ -17,17 +17,20 @@ void entTamanho(int* lin1, int* col1, int* lin2, int* col2){
 
 int** instMatPonteiro(int lin, int col){
     int** mat = malloc(lin * sizeof(int*));
-    for(int i = 0; i < col; i++)
-        mat[i] = malloc(col * sizeof(int*));
+    for(int i = 0; i < lin; i++){
+        mat[i] = malloc(col * sizeof(int));
+        for(int j = 0; j < col; j++){
+            mat[i][j] = 0;
+        }
+    }
 
     return mat;
-
 }
 
 void entMatPonteiro(int** mat, int lin, int col){
     for(int i = 0; i < lin; i++){
         for(int j = 0; j < col; j++){
-            printf("Entre o elemento [%d][%d] da matriz\n>>>", i + 1, j + 1);
+            printf("Entre o elemento [%d][%d] da matriz 1\n>>>", i + 1, j + 1);
             scanf("%d", &mat[i][j]);
         }
     }
@@ -39,7 +42,7 @@ void saidaMatPonteiro(int** mat, int lin, int col){
             printf("%d ", mat[i][j]);
         printf("\n");
     }
-
+    printf("\n");
 }
 
 int* instMatSimples(int lin, int col){
@@ -52,7 +55,7 @@ int* instMatSimples(int lin, int col){
 void entMatSimples(int* mat, int lin, int col){
     for(int i = 0; i < lin; i++){
         for(int j = 0; j < col; j++){
-            printf("Entre o elemento [%d][%d] da matriz\n>>> ", i + 1, j + 1);
+            printf("Entre o elemento [%d][%d] da matriz 2\n>>> ", i + 1, j + 1);
             scanf("%d", &mat[(i * col) + j]);
         }
     }
@@ -64,18 +67,17 @@ void saidaMatSimples(int* mat, int lin, int col){
             printf("%d ", mat[(i * col) + j]);
         printf("\n");
     }
+    printf("\n");
 }
 
-int** multiplicaMatrizes(int** mat1, int* mat2, int lin1, int col1, int lin2, int col2){
+int** multipicaMatrizes(int** mat1, int* mat2, int lin1, int col1, int lin2, int col2){
     int** matF = instMatPonteiro(lin1, col2);
     for(int i = 0; i < lin1; i++){
         for(int j = 0; j < col2; j++){
             for(int k = 0; k < lin2; k++){
                 matF[i][j] += mat1[i][k] * mat2[(k * col2) + j];
             }
-            printf("%d ", matF[i][j]);
         }
-        printf("\n");
     }
     return matF;
 }
@@ -93,10 +95,10 @@ int main(int arc, char* argv[]){
         entMatSimples(matS, lin2, col2);
 
         saidaMatPonteiro(matP, lin1, col1);
+
         saidaMatSimples(matS, lin2, col2);
-        int** matF = multipicaMatrizes(matS, matP, lin1, col1, lin2, col2);
-        
-    
+        int** matF = multipicaMatrizes(matP, matS, lin1, col1, lin2, col2);
+        saidaMatPonteiro(matF, lin1, col2);
         free(matF);
     }
     else    
