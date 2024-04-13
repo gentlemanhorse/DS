53e6df6f372d8da97e47c5692acbbc0aa963cb4a
gentlemanhorse <138687107+gentlemanhorse@users.noreply.github.com>
2024-04-13 14:27:04 -0300

    Add files via upload

--- stack/array_stack.c
+++ stack/array_stack.c
@@ -11,6 +11,7 @@ void push(Stack *s, float v) {
         printf("Pilha atingiu a capacidade máxima\n");
         exit(1);
     }
+    printf("%d valor = %f\n", s->n, v);
     s->array[s->n++] = v;
 }
 
@@ -29,15 +30,23 @@ int is_empty(Stack *s) {
 }
 
 void free_stack(Stack *s) {
-    free(p);
+    free(s);
 }
 
 int testa_mais_elementos(Stack *s1, Stack *s2) {
-
+    return (s1->n > s2->n);
 }
 
 void transferir_elementos(Stack *s1, Stack *s2);
 
-void inverter(Stack *s);
+void inverter(Stack *s) {
+    Stack *t = create_stack();
+    while(!is_empty(s)) {
+        push(t, pop(s));
+    }
+    free_stack(s);
+    s = t;
+
+}
 
 int iguais(Stack *s1, Stack *s2);
---
+++ stack/ex1.c
@@ -0,0 +1,26 @@
+#include "array_stack.h"
+
+int main(int argc, char *argv[]) {
+    Stack *s1 = create_stack();
+    Stack *s2 = create_stack();
+    printf("Stack 1\n");
+    for(int i = 0; i < 10; i++) 
+	    push(s1, 1.9 * i);
+    
+    printf("\n\nStack 2\n");
+    for(int i = 0; i < 20; i++)
+	    push(s2, 12.2 * i);
+ 
+    printf("\n\n");
+    if(testa_mais_elementos(s1, s2)) 
+        printf("Stack 1 contem mais elementos que a stack 2.\n");
+
+    else 
+        printf("Stack 2 contem mais elementos que a stack 1.\n");
+    
+    printf("Stack invertida:\n");
+    inverter(s1);
+    free_stack(s1);
+    free_stack(s2);
+    return 0;
+}
